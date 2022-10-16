e1a42f159ccdbd14c10e282513050fb18c2fd862
SIMRAN88e <rajsimran1996@gmail.com>
2022-10-16 20:08:26 +0530

    fix: icon addition in header

--- src/component/tracks/Search.js
+++ src/component/tracks/Search.js
@@ -35,7 +35,7 @@ class Search extends Component {
                     return(
                         <div className="card card-body mb-4 p-4">
                             <h1 className="display-4 text-center">
-                                <i className="fas fa-music"></i> Search for a Song
+                                <i className="fas fa-music"></i> Search for a Song<i className="fas fa-music"></i>
                             </h1>
                             <p className="lead text-center">Get the lyrics for any song</p>
                             <form onSubmit={this.findTrack.bind(this, dispatch)}>
