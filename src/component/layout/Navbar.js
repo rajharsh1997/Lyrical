7fd846683444b8d2b622448e1aae2b6f29d7d923
SIMRAN88e <rajsimran1996@gmail.com>
2022-10-16 20:15:46 +0530

    fix: added a new spinner gif

--- src/component/layout/Navbar.js
+++ src/component/layout/Navbar.js
@@ -4,7 +4,6 @@ const Navbar = () => {
     return (
         <nav className="navbar navbar-dark bg-dark mb-5">
             <span className="navbar-brand mb-0 h1 mx-auto">Lyrical</span>
-            
         </nav>
     );
 };
